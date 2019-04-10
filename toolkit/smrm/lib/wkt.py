a3f72fadcae581f8db9a5d92d1ae659f111009f5
Felix Cornelius <mail@felixcornelius.de>
2019-04-10 14:56:29 +0200

    add method to write wkt points

--- toolkit/smrm/lib/wkt.py
+++ toolkit/smrm/lib/wkt.py
@@ -1,15 +1,16 @@
 # -*- coding: utf-8 -*-
 from typing import List, Tuple
-WKT_BEGIN = 'LINESTRING ('
+WKT_BEGIN_LNSTR = 'LINESTRING ('
+WKT_BEGIN_POINT = 'POINT ('
 WKT_END = ')'
 WKT_CRD_SEP = ' '
 WKT_PNT_SEP = ', '
 
-def write_wkt(coords: List[Tuple[float, float]], file: str, append=False):
+def write_linestring(coords: List[Tuple[float, float]], file: str, append=False) -> None:
     content = ''
     if append:
         content += '\n\n'
-    content += WKT_BEGIN
+    content += WKT_BEGIN_LNSTR
 
     for i, c in enumerate(coords):
         content += str(c[0]) + WKT_CRD_SEP + str(c[1])
@@ -17,6 +18,21 @@ def write_wkt(coords: List[Tuple[float, float]], file: str, append=False):
             content += WKT_PNT_SEP
 
     content += WKT_END
+
+    with open(file, 'a+' if append else 'w+') as fp:
+        fp.write(content)
+
+def write_points(coords: List[Tuple[float, float]], file: str, append=False):
+    content = ''
+    if append:
+        content += '\n\n'
+
+    for i, c in enumerate(coords):
+        content += WKT_BEGIN_POINT
+        content += str(c[0]) + WKT_CRD_SEP + str(c[1])
+        content += WKT_END
+        if i < len(coords) - 1:
+            content += '\n\n'
 
     with open(file, 'a+' if append else 'w+') as fp:
         fp.write(content)
