bdacb42296e8ee808bd9aefa781c0455d1f8f3c5
Felix Cornelius <mail@felixcornelius.de>
2019-05-09 00:14:56 +0200

    read in gtfs file and query durations and schedules

--- toolkit/smrm/scenario.py
+++ toolkit/smrm/scenario.py
@@ -1,18 +1,20 @@
 # -*- coding: utf-8 -*-
 import sys, os, math
 from os import path
+from pathlib import Path
 from lib.osm import OsmRouteParser
 from lib.project import Projector
 from lib.one import HostGroup, ScenarioSettings
-from lib import out
+from lib.gtfs import GTFSReader
+from lib import writer
 
-PARENT_DIR = '..'
 DATA_DIR = 'data'
 NODES_FILE = '{}_nodes.wkt'
 STOPS_FILE = '{}_stops.csv'
+SCHEDULE_FILE = '{}_schedule.csv'
 STATIONS_FILE = 'stations.wkt'
 
-def main(osm_file):
+def main(osm_file, gtfs_file):
     scenario = basename_without_ext(osm_file)
 
     # read all routes (relations with tag[k="type"][v="route"])
@@ -29,15 +31,24 @@ def main(osm_file):
     proj = Projector(precision=2)
     width, height = proj.init_dimensions(points)
 
+    # open GTFS zip, pass the osm routes as reference trips
+    # and query the schedules and durations of all routes
+    gtfs = GTFSReader(gtfs_file)
+    ref_routes = [(r.name, r.first, r.last, len(r.stops)) for r in routes]
+    gtfs.set_ref_trips(ref_routes)
+    schedule = gtfs.schedule(weekday_type=0, max_exceptions=180)
+    durations = gtfs.trip_durations()
+
     # switch to ONE project root and make dir
     # for the new scenario in /data
-    os.chdir(path.join(PARENT_DIR, PARENT_DIR))
-    out_dir = path.join(DATA_DIR, scenario)
+    one_dir = Path.cwd().parent.parent
+    out_dir = path.join(one_dir, DATA_DIR, scenario)
     if not path.isdir(out_dir):
         os.mkdir(out_dir)
 
     nodes_file = path.join(out_dir, NODES_FILE)
     stops_file = path.join(out_dir, STOPS_FILE)
+    schedule_file = path.join(out_dir, SCHEDULE_FILE)
     stations_file = path.join(out_dir, STATIONS_FILE)
 
     # begin contents of a ONE settings file for this scenario
@@ -49,14 +60,9 @@ def main(osm_file):
         # to x,y tuples on projection pane
         nodes = proj.transform_coords(r.nodes)
         stops = proj.transform_coords(r.stops)
-        name = r.name
 
-        # write a wkt LINESTRING with all nodes in this route
-        # (includes stops and way nodes)
-        out.write_wkt_linestring(nodes, nodes_file.format(name))
-        # write stops in csv with 1st col: stop coords,
-        # 2nd col: time to each stop
-        out.write_csv_stops(stops, stops_file.format(name))
+        name = r.name
+        stations.extend(stops)
 
         # for each route, create a host group.
         # this group will contain the moving hosts along the route.
@@ -69,12 +75,34 @@ def main(osm_file):
         g.set_okmap(nodes_file.format(name))
         s.add_group(g)
 
-        stations.extend(stops)
+        # write a wkt LINESTRING with all nodes in this route
+        # (includes stops and way nodes)
+        writer.write_wkt_linestring(
+            coords=nodes,
+            file=nodes_file.format(name)
+        )
+
+        # write only stop nodes in csv with
+        # 1st col: stop coords
+        # 2nd col: durations between each stop to the next one
+        # (will be used to generate path speeds)
+        writer.write_csv_stops(
+            coords=stops,
+            durations=durations.get(r.name),
+            file=stops_file.format(name)
+        )
+
+        # write the schedule of this line
+        # (start times, start and stop ids and direction)
+        writer.write_csv_schedule(
+            schedule=schedule.get(r.name),
+            file=schedule_file.format(name)
+        )
 
     # add another group for all stations.
     # in this group, for each station one stationary host
-    # will be spawned to act as a fixed relay node at the platform
-    out.write_wkt_points(stations, stations_file)
+    # will be created to act as a fixed relay node at the platform
+    writer.write_wkt_points(stations, stations_file)
     g = HostGroup('S')
     g.set('movementModel', 'StationaryMultiPointMovement')
     g.set('stationarySystemNr', 1)
@@ -109,8 +137,8 @@ def basename_without_ext(file: str) -> str:
 
 
 if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        print("Please give .osm input file as argument")
+    if len(sys.argv) < 3:
+        print("Please give .osm and .zip input files as arguments")
         sys.exit(1)
 
-    main(sys.argv[1])
+    main(sys.argv[1], sys.argv[2])
