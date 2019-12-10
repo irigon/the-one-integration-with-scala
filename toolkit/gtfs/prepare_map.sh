440b4282fe82eca205bf1bcb3a0ec2ec8792e038
simulant <jose.irigon@tu-dresden.de>
2019-12-10 17:16:27 +0100

    updating prepare maps

--- toolkit/gtfs/prepare_map.sh
+++ toolkit/gtfs/prepare_map.sh
@@ -14,8 +14,16 @@ err_(){
 	exit 1
 }
 
+get_osm(){
+	_OSM="$(find . -name "$SCENARIO.osm" -print -quit)"
+	[[ "$_OSM" != "" ]] && echo "$_OSM" && return
+	_OSM_PBF="$(find . -name "$SCENARIO.osm.pbf" -print -quit)"
+	[[ "$_OSM_PBF" != "" ]] && echo "$_OSM_PBF" && return
+	echo ""
+}
+
 ### get filename
-if [ "$#" -ne 1 ] ; then  
+if [ "$#" -lt 1 ] ; then  
 	usage 
 	err_ "Wrong number of parameters. Expected: scenario name"
 fi
@@ -25,6 +33,11 @@ BASE_DIR="$(dirname $me)"
 SCENARIO=$1
 MAPS_DIR=$BASE_DIR/maps
 DST_SCENARIO_DIR="$MAPS_DIR/$SCENARIO"
+DEFINITIONS=map_definitions
+
+
+## Import source description
+source $DEFINITIONS/$SCENARIO
 
 ### Does the directory exists?
 if [ -d "$DST_SCENARIO_DIR" ]; then
@@ -37,25 +50,37 @@ if [ -d "$DST_SCENARIO_DIR" ]; then
 	fi
 fi
 
-### Variable definitions
-OSM_PBF=$(find . -name "$SCENARIO.osm.pbf" -print -quit)
-OSM=$(find . -name "$SCENARIO.osm" -print -quit)
+OSM="$(get_osm)"
 GTFS=$(find . -name "$SCENARIO.zip" -print -quit)
 
-[[ -f "$OSM_PBF" || -f "$OSM" ]] || err "OSM file could not be found."
-[ -f "$GTFS" ] || err "OSM file could not be found."
-
 # create directories
-mkdir -p $MAPS_DIR/$SCENARIO
+mkdir -p "$MAPS_DIR/$SCENARIO"
+
+# If the OSM file is not declared, try to download it
+# respecting its extension (.osm or .osm.pbf)
+if [[ "$OSM" == "" ]] ; then
+	_base=$(basename ${OSM_source})
+	_extension=".${_base#*osm.}"
+	[[ "$_extension" == "." ]] && _extension=""
+	wget -O "$MAPS_DIR/$SCENARIO/$SCENARIO.osm${_extension}" "$OSM_source"
+	OSM="$(get_osm)"
+	[[ ! -f "$OSM" ]] && err "OSM file could not be found."
+fi
+if [[ "$GTFS" == "" ]] ; then
+	extension="zip"
+	wget -O "$MAPS_DIR/$SCENARIO/$SCENARIO.${extension}" "$GTFS_source"
+	[ -f "$MAPS_DIR/$SCENARIO/$SCENARIO.${extension}" ] || err "GTFS file could not be found."
+fi
 
 # if osm is compressed, unpack
 [ ! -f "osmconvert" ] && $(curl http://m.m.i24.cc/osmconvert.c | cc -x c - -lz -O3 -o osmconvert)
-if [ ! -z "$OSM_BPF" ] ; then 
-       ./osmconvert "$OSM_BPF" > $DST_SCENARIO_DIR/$SCENARIO.osm
+if [[ "$OSM" =~ .*pbf$  ]] ; then 
+       ./osmconvert "$OSM" > $DST_SCENARIO_DIR/$SCENARIO.osm
 else
 	cp -f $OSM $DST_SCENARIO_DIR || true
 fi
 cp -f $GTFS "$MAPS_DIR/$SCENARIO" || true
+exit
 
 # Merge with pfaedle
 pushd $DST_SCENARIO_DIR
